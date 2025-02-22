98d247773a839d8d709907fbadca98353503e841
fucloud-macbook <270928474@qq.com>
2025-02-22 16:02:41 +0800

    feat: 实现 write_file 写文件的能力

--- src/moman-bin/utils.py
+++ src/moman-bin/utils.py
@@ -11,6 +11,11 @@ def read_yaml(file_path: Path) -> Dict:
         return result
 
 
+def write_file(file_path: Path, data: str):
+    with open(file_path, "w", encoding="utf-8") as f:
+        f.write(data)
+
+
 def write_yaml(file_path: Path, data: Dict[str, Any]):
     with open(file_path, "w", encoding="utf-8") as f:
         yaml.dump(data, f)
