0f6319d8e7f15a4819424ba69556c102fae28986
fucloud-macbook <270928474@qq.com>
2025-03-07 11:16:14 +0800

    feat: 实现 moman 执行脚本

--- src/moman-bin/main.py
+++ src/moman-bin/main.py
@@ -1,27 +1,9 @@
-import argparse
-# import sys
-
-from pathlib import Path
-
-from handler import MomanCmdHandler
-
-
-def test(args):
-    print(args)
+from moman_bin.cli import MomanCliExecutor
 
 
 def main():
-    parser = argparse.ArgumentParser()
-
-    sub_parsers = parser.add_subparsers()
-
-    parser_modular = sub_parsers.add_parser("modular")
-    parser_modular.set_defaults(func=test)
-
-    MomanCmdHandler.modular(Path("/Users/fucloud/Code/moman-demo"))
-
-    # args = parser.parse_args(sys.argv[1:])
-    # args.func(args)
+    executor = MomanCliExecutor()
+    executor.exec()
 
 
 if __name__ == "__main__":
